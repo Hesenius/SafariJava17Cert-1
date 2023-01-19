c029a78da228fe761c680a62015f29637f45fcbf
simon <simon@dancingcloudservices.com>
2023-01-19 13:01:01 -0700

    Day 2 Long question!

--- src/main/java/casting/ex2/Ex2.java
+++ src/main/java/casting/ex2/Ex2.java
@@ -2,16 +2,26 @@ package casting.ex2;
 
 public class Ex2 {
   public static void main(String[] args) {
-    Base b = new Sub();
+    Base b = null;//new Sub();
 //    ((StringBuilder) b) // line n1
 //        .length();      // line n2
+    ((Runnable)b).run();
   }
 }
 
 class Base {
   void doBaseStuff() { System.out.println("doBaseStuff"); }
 }
-class Sub extends Base {
+
+final class Sub extends Base {
   void doBaseStuff() { System.out.println("doSubStuff"); }
   void length() { System.out.println("doLengthStuff"); }
 }
+
+//class Sub2 extends Base implements Runnable {
+//
+//  @Override
+//  public void run() {
+//    System.out.println("Hahah! I'm Runanble");
+//  }
+//}
